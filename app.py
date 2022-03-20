fe3cd1b88c67b8ac050ed4f2cf90553f4605c1c6
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2022-03-20 18:26:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-import cv2
+import os
 import numpy as np
 from flask import Flask, render_template, Response
 from attendance import show_vid
 
 app = Flask(__name__)
-
-cap = cv2.VideoCapture(0)
-
-
-def gen_frame():
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
@@ -31,4 +17,5 @@ def video_feed():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
